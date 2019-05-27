19c92f3d2ec7195d70dfb03d447d32989e80859c
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-05-27 13:08:07 +0100

    Load assets.

--- includes/classes/Page.php
+++ includes/classes/Page.php
@@ -32,6 +32,7 @@ class Page {
 	 */
 	public function hooks() {
 		add_action( 'admin_menu', [ $this, 'admin_menu' ], 20 );
+		add_action( 'admin_enqueue_scripts', [ $this, 'assets' ] );
 	}
 
 	/**
@@ -41,6 +42,20 @@ class Page {
 	 */
 	public function admin_menu() {
 		add_submenu_page( 'edit.php?post_type=listings', esc_html__( 'Posterno Extensions', 'posterno' ), esc_html__( 'Extensions', 'posterno' ), 'manage_options', 'posterno-addons', [ $this, 'display' ] );
+	}
+
+	/**
+	 * Load assets.
+	 *
+	 * @return void
+	 */
+	public function assets() {
+
+		$screen = get_current_screen();
+
+		if ( $screen->id === 'edit-listings' ) {
+			wp_enqueue_style( 'posterno-addons-listings-table', PNO_PLUGIN_URL . 'vendor/posterno/addons/dist/css/list-table.css', false, PNO_VERSION );
+		}
 	}
 
 	/**
